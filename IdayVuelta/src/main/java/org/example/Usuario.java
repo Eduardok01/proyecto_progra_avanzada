2b5af60f23d2955cb37cf96b7690e90be1340f7c
MarceloRebolledo <m.rebolledo17@ufromail.cl>
2024-10-11 16:50:39 -0300

    Actualización código y javadoc Correo y Usuario

--- IdayVuelta/src/main/java/org/example/Usuario.java
+++ IdayVuelta/src/main/java/org/example/Usuario.java
@@ -1,5 +1,11 @@
 package org.example;
 
+/**
+ * Esta clase representa un usuario en el sistema.
+ * Contiene la información personal del usuario.
+ * @author Marcelo Rebolledo
+ */
+
 public class Usuario {
 
 	private String nombre;
@@ -7,6 +13,9 @@ public class Usuario {
 	private String telefono;
 	private String email;
 
+	/**
+	 * Método para ingresar los datos personales del usuario.
+	 */
 	public void ingresaDatosPersonales() {
 		// TODO - implement Usuario.ingresaDatosPersonales
 
@@ -16,6 +25,9 @@ public class Usuario {
 		this.email = email;
 	}
 
+	/**
+	 * Actualizar los datos personales del usuario.
+	 */
 	public void actualizarDatos() {
 		// TODO - implement Usuario.actualizarDatos
 
@@ -24,6 +36,13 @@ public class Usuario {
 		this.email = email;
 	}
 
+	/**
+	 * Crear un nuevo usuario con los datos proporcionados.
+	 *@param nombre nombre del usuario.
+	 *@param rut el RUT del usuario.
+	 *@param telefono número de telefono del usuario.
+	 *@param email correo del usuario.
+	 */
 	public Usuario(String nombre, String rut, String telefono, String email) {
 		this.nombre = nombre;
 		this.rut = rut;
@@ -31,34 +50,74 @@ public class Usuario {
 		this.email = email;
 	}
 
+	/**
+	 * Obtiene el nombre del usuario.
+	 *
+	 * @return El nombre del usuario.
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Obtiene el RUT del usuario.
+	 *
+	 * @return El RUT del usuario.
+	 */
 	public String getRut() {
 		return rut;
 	}
 
+	/**
+	 * Obtiene el número de teléfono del usuario.
+	 *
+	 * @return El número de teléfono del usuario.
+	 */
 	public String getTelefono() {
 		return telefono;
 	}
 
+	/**
+	 * Obtiene el correo electrónico del usuario.
+	 *
+	 * @return El correo electrónico del usuario.
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * Establece el nombre del usuario.
+	 *
+	 * @param nombre El nombre a establecer.
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Establece el RUT del usuario.
+	 *
+	 * @param rut El RUT a establecer.
+	 */
 	public void setRut(String rut) {
 		this.rut = rut;
 	}
 
+	/**
+	 * Establece el número de teléfono del usuario.
+	 *
+	 * @param telefono El número de teléfono a establecer.
+	 */
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
 
+	/**
+	 * Establece el correo electrónico del usuario.
+	 *
+	 * @param email El correo electrónico a establecer.
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
