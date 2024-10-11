cb494f02a04d620515a25ff8b255870aa0aa76e9
Fernandobhch4 <f.hernandez10@ufromail.cl>
2024-10-11 12:32:28 -0300

    add clase bus

--- IdayVuelta/src/main/java/org/example/Bus.java
+++ IdayVuelta/src/main/java/org/example/Bus.java
@@ -1,25 +1,66 @@
-package org.example;
-
+import java.util.ArrayList;
 import java.util.List;
-//hola
-public class Bus {
 
+public class Bus {
 	private String idBus;
 	private int capacidad;
-	private List<Asiento> listaAsiento;
+	private List<Asiento> listaAsientos;
 
-	public List<Asiento> verificarDisponibilidadAsiento() {
-		// TODO - implement Bus.verificarDisponibilidadAsiento
-		throw new UnsupportedOperationException();
+	public Bus(String idBus, int capacidad) {
+		this.idBus = idBus;
+		this.capacidad = capacidad;
+		this.listaAsientos = new ArrayList<>();
+		for (int i = 1; i <= capacidad; i++) {
+			listaAsientos.add(new Asiento(i));
+		}
 	}
 
-	/**
-	 * 
-	 * @param numeroAsiento
-	 */
+	public List<Asiento> verificarDisponibilidadAsientos() {
+		List<Asiento> asientosDisponibles = new ArrayList<>();
+		for (Asiento asiento : listaAsientos) {
+			if (!asiento.isOcupado()) {
+				asientosDisponibles.add(asiento);
+			}
+		}
+		return asientosDisponibles;
+	}
+
 	public Asiento asignarAsiento(int numeroAsiento) {
-		// TODO - implement Bus.asignarAsiento
-		throw new UnsupportedOperationException();
+		if (numeroAsiento > 0 && numeroAsiento <= capacidad) {
+			Asiento asiento = listaAsientos.get(numeroAsiento - 1);
+			if (!asiento.isOcupado()) {
+				asiento.reservarAsiento();
+				return asiento;
+			} else {
+				System.out.println("El asiento ya está ocupado.");
+			}
+		} else {
+			System.out.println("Número de asiento inválido.");
+		}
+		return null;
 	}
 
+	public String getIdBus() {
+		return idBus;
+	}
+
+	public void setIdBus(String idBus) {
+		this.idBus = idBus;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public List<Asiento> getListaAsientos() {
+		return listaAsientos;
+	}
+
+	public void setListaAsientos(List<Asiento> listaAsientos) {
+		this.listaAsientos = listaAsientos;
+	}
 }
