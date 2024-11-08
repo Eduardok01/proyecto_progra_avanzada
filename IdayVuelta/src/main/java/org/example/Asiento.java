1107271854ecf5467c577b33bf11642058141925
Your Name <your_email@example.com>
2024-11-08 18:18:13 -0300

    Update Asiento.java

--- IdayVuelta/src/main/java/org/example/Asiento.java
+++ IdayVuelta/src/main/java/org/example/Asiento.java
@@ -8,9 +8,9 @@ package org.example;
  */
 public class Asiento {
 
+    private int numero;
     private Usuario usuario;
-    private int numeroAsiento;
-    private Boolean estado;
+    private Boolean ocupado;
 
     /**
      * Constructor de la clase Asiento.
@@ -19,68 +19,25 @@ public class Asiento {
      * @param numeroAsiento Número del asiento.
      */
     public Asiento(int numeroAsiento) {
-        this.numeroAsiento = numeroAsiento;
-        this.estado = false;
+        this.numero = numeroAsiento;
+        this.usuario = null;
+        this.ocupado = false;
     }
 
-    /**
-     * Obtiene el número del asiento.
-     *
-     * @return El número del asiento.
-     */
-    public int getNumeroAsiento() {
-        return numeroAsiento;
+    public int getNumero() {
+        return numero;
     }
 
-    /**
-     * Establece un nuevo número para el asiento.
-     *
-     * @param numeroAsiento Nuevo número para el asiento.
-     */
-    public void setNumeroAsiento(int numeroAsiento) {
-        this.numeroAsiento = numeroAsiento;
+    public Usuario getUsuario() {
+        return usuario;
     }
 
-    /**
-     * Obtiene el estado actual del asiento.
-     *
-     * @return El estado del asiento: true si está ocupado, false si está libre.
-     */
-    public Boolean getEstado() {
-        return estado;
+    public Boolean isOcupado() {
+        return ocupado;
     }
 
-    /**
-     * Establece el estado del asiento.
-     *
-     * @param estado El nuevo estado del asiento: true para ocupado, false para libre.
-     */
-    public void setEstado(Boolean estado) {
-        this.estado = estado;
+    public void reservarAsiento(Usuario usuario) {
+        this.usuario = usuario;
+        this.ocupado = true;
     }
-
-    /**
-     * Reserva el asiento, cambiando su estado a ocupado.
-     *
-     * @return true si el asiento fue reservado con éxito, false si no se pudo reservar.
-     * @throws UnsupportedOperationException Método no implementado.
-     */
-    public Boolean reservarAsiento(Usuario usuario) {
-        if (!estado) {
-            estado = true;
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Libera el asiento, cambiando su estado a libre.
-     *
-     * @throws UnsupportedOperationException Método no implementado.
-     */
-    public void liberarAsiento() {
-        estado = false;
-    }
-
-
 }
