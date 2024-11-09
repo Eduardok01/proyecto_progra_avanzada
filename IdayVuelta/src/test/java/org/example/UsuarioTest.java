d9def5566a084533775877a3e565fa5fb9590127
MarceloRebolledo <m.rebolledo17@ufromail.cl>
2024-11-09 20:27:15 -0300

    Actualización código

--- IdayVuelta/src/test/java/org/example/UsuarioTest.java
+++ IdayVuelta/src/test/java/org/example/UsuarioTest.java
@@ -8,11 +8,17 @@ class UsuarioTest {
     @Test
     void testActualizarDatos() {
         Usuario usuario = new Usuario("Marcelo", "12345678-9", "987654321", "marcelo@gmail.com");
-        usuario.actualizarDatos("Carlos","20105304-1","912345678","carlos@gmail.com");
+        usuario.actualizarDatos("Carlos","20105304-1","452345678","carlos@gmail.com");
 
         assertEquals("Carlos", usuario.getNombre());
         assertEquals("20105304-1",usuario.getRut());
-        assertEquals("912345678", usuario.getTelefono());
+        assertEquals("452345678", usuario.getTelefono());
         assertEquals("carlos@gmail.com", usuario.getEmail());
+
+        System.out.println("Datos del usuario actualizados:");
+        System.out.println("Nombre: " + usuario.getNombre());
+        System.out.println("RUT: " + usuario.getRut());
+        System.out.println("Teléfono: " + usuario.getTelefono());
+        System.out.println("Email: " + usuario.getEmail());
     }
 }
